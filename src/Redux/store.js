b467b2bcd1b34294714269c386f3d8552e65fda6
ahsankhan201010 <ahsankhan201010@gmail.com>
2020-12-10 23:03:08 +0500

    integrated redux

---
+++ src/Redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+
+var middleware = [thunk]
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+));
+
+export default store;
